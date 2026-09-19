56310a782e8124d5f11d2ac0645953027619e9cd
ssmoot813 <smo24001@byui.edu>
2026-09-19 02:56:28 -0600

    Completed Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,59 @@
-using System;
+Console.Write("What is your grade percentage? ");
+string input = Console.ReadLine();
+int grade = int.Parse(input);
 
-class Program
+string letter;
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    letter = "A";
+}
+else if (grade >= 80)
+{
+    letter = "B";
+}
+else if (grade >= 70)
+{
+    letter = "C";
+}
+else if (grade >= 60)
+{
+    letter = "D";
+}
+else
+{
+    letter = "F";
+}
+
+string sign = "";
+int lastDigit = grade % 10;
+
+if (lastDigit >= 7)
+{
+    sign = "+";
+}
+else if (lastDigit < 3)
+{
+    sign = "-";
+}
+
+if (letter == "A" && sign == "+")
+{
+    sign = "";
+}
+
+if (letter == "F")
+{
+    sign = "";
+}
+
+Console.WriteLine($"Your grade is: {letter}{sign}");
+
+if (grade >= 70)
+{
+    Console.WriteLine("Congratulations, you passed the course!");
+}
+else
+{
+    Console.WriteLine("You did not pass this time. Keep working hard and you'll get there!");
 }
